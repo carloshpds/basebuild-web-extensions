f64114174ba3dbd48d871a687fd022cf12092426
Carlos Henrique <carloshpds@gmail.com>
2024-03-16 13:37:48 -0300

    feat(basebuild): common vite settings in main module

--- src/apps/module/index.ts
+++ src/apps/module/index.ts
@@ -1,1 +1,12 @@
-export { default as config } from '@/../vite.config.js'
+import { UserConfig as ViteUserConfig } from 'vite'
+import { defineConfig } from 'vite'
+import bbWebExtensionsConfig from './vite.config'
+
+export { bbWebExtensionsConfig }
+
+export const bbWebExtensions = (viteConfig: ViteUserConfig = {}) => {
+  return defineConfig({
+    ...bbWebExtensionsConfig,
+    ...viteConfig
+  })
+}
