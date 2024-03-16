8aa1ade5ec9a4d94fc1dd16d420a273ad72ae42e
Carlos Henrique <carloshpds@gmail.com>
2024-03-16 13:38:25 -0300

    test(demo): importing basebuild in to demo project

--- src/apps/demo/vite.config.ts
+++ src/apps/demo/vite.config.ts
@@ -1,7 +1,5 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+// @ts-ignore
+import { bbWebExtensions } from '../../../dist/src/apps/module/index.js'
+console.log('bbWebExtensions', bbWebExtensions)
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default bbWebExtensions()
